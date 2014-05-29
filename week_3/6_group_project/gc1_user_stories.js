b6270df8187702236188e3d2c40c925c91236f98
LoRyder1 <lspnag@gmail.com>
2014-05-29 08:57:37 -0700

    update

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -1,25 +1,16 @@
 
-<!DOCTYPE html>
-<html>
-   <head>
-    <title>User stories</title>
-    
-   </head>
-   <body>
-<h1>USer stories for week3 group Projecto</h1>
-
-<p> I have one group of numbers: 1, 2, 3, 4, 5, 5, 7
+// User Stories for Group Projecto week 3
+<!--
+I have one group of numbers: 1, 2, 3, 4, 5, 5, 7
 I have second group of numbers: 4, 4, 5, 5, 6, 6, 6, 7
 
-
- <ol>
-<li>I want to be able to get the sum of each group separately.</li>
-<li>I want to be able to get the mean of each group separately. </li>
-<li>I want to be able to get the median of each group separtely.</li>
-
-</ol>
+1. I want to be able to get the sum of each group separately.
+2. I want to be able to get the mean of each group separately. 
+3. I want to be able to get the median of each group separtely.
 
 
-</p>
- </body>
-</html>
+
+
+
+//-->
+
