96cf635b0a10197a4cf03c5ca3180d7ede21c8b1
LoRyder1 <lspnag@gmail.com>
2014-06-09 12:15:42 -0700

    update to 3_6

--- week_3/6_group_project/my_solution.js
+++ week_3/6_group_project/my_solution.js
@@ -10,21 +10,6 @@
 // 2. I want to be able to get the mean of each group separately. 
 // 3. I want to be able to get the median of each group separately.
 
-
-// User Stories for Group Projecto week 3
-// I have one group of numbers: 1, 2, 3, 4, 5, 5, 7
-// I have second group of numbers: 4, 4, 5, 5, 6, 6, 6, 7
-
-// 1. I want to be able to get the sum of each group separately.
-// 2. I want to be able to get the mean of each group separately. 
-// 3. I want to be able to get the median of each group separately.
-
-
-
-
-
-
-
 // Our role in the group is: translate pseudocode into actual code.
 
 // pseudocode
