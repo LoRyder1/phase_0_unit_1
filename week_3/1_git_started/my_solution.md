aa4a768cec73154d4f5610451570a50fa9e0cf0b
LoRyder1 <lspnag@gmail.com>
2014-06-08 19:29:39 -0700

    work

--- week_3/1_git_started/my_solution.md
+++ week_3/1_git_started/my_solution.md
@@ -3,53 +3,75 @@ Define the following commands and describe how they work/what they do.
 
 
 #### add
-<!-- Your defnition here -->
+
+<p>add one or more files to staging (index)
+</p>
 
 #### branch
-<!-- Your defnition here -->
+
+<p> list all branches in your repo , and also tell you what branch you're currently in
+</p>
 
 #### checkout
-<!-- Your defnition here -->
+<p> create a new branch and switch to it
+</p>
 
 #### clone
-<!-- Your defnition here -->
+<p> Create a working copy of a local repository
+</p>
 
 #### commit
-<!-- Your defnition here -->
-
+<p> Commit changes to head (but not yet to the remote repository)
+</p>
 #### fetch
-<!-- Your defnition here -->
-
+<p> fetch  the latest history from the server
+</p>
 #### log
-<!-- Your defnition here -->
-
+<p> find specific commits in your project history
+</p>
 #### merge
-<!-- Your defnition here -->
+<p> merge a branch context into your current main branch
+	</p>
 
 #### pull
-<!-- Your defnition here -->
+<p> will run a git fetch immediately followed by a git merge
+</p>
 
 #### push
-<!-- Your defnition here -->
+<p> push your new branches and data to a remote repository
+</p>
 
 #### reset
-<!-- Your defnition here -->
+<p> undo changes and commits
+</p>
+
 
 #### rm
-<!-- Your defnition here -->
+<p> will remove entries from the staging area
+	</p>
+
 
 #### status
-
+<p> view the status of your files in the working directory and staging area
+</p>
 
 ## Release 4: Git Workflow
 
 - Push files to a remote repository
-- Fetch changes
-- Commit locally
 
+<p> git add , git commit -m "message", git push
+</p>	
+- Fetch changes
+<p> git fetch upstream, git merge upstream/master
+	</p>
+- Commit locally
+<p> git add, git commit -m "message"
 ## Release 5: Errors you encountered
 1. Error
 2. Solution
 3. Cause (explain the error)
 
 ## Release 6: Reflection
+<p> Using the terminal is relatively easy I had no real problems using the terminal. The hard part is just memorizing the commands so they are readily available and also to know what they do.
+I just ran into a problem where it can not commit or add files into the staging area. I was going to try to resave and then try again.  
+</p>
--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,147 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// Rescue mission
+// delete move right
+// move up
+// move right
+// move down
+// attack
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+// Grab the mushroom!
+// move up 
+// mover right
+// move left
+// move up 
+// attack
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+this.attackNearbyEnemy();
  
+// Drink Me
+// move right
+// attack 
+// move right
+// move down
+// move up
+// move right
+// attack
+this.moveRight();
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+
+// Taunt the Guards
+// move right
+// move right
+// move up
+// move right
+// say something
+// move down
+// move right
+// move up
+// move right
+
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.say("whats cracking homey!");
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+
+// It's a trap
+// down 
+// say anything
+// up
+
+this.moveDown();
+this.moveDown();
+this.say("Heeeeeeeey");
+this.moveUp();
+this.moveUp();
+
+// Break the Prison
+// If I know the person return true
+// otherwise return false
+
+if(name ==="name")
+	return true;
+if(name ==="name")
+	return true;
+if(name ==="name")
+	return true;
+if(name ==="name")
+	return true;
+return false;
+
+// Taunt
+// yell something
+this.say("dude");
+this.say("hmmm dude");
+this.say("@!$!@3 dude");
+
+// Cowardly Taunt
+// move up left
+// type something
+// move down right
+this.moveXY(X,Y);
+this.say("saying something");
+this.moveXY(X,Y);
+
+// Commanding Followers
+// say something 
+// move down left
+// say something and attack
+this.say("due man");
+this.moveXY(X,Y);
+this.say("attack players");
+
+// Mobile Artillery
+// move 
+// attack 
+// attack
+// attack
+this.moveXY(X,Y);
+this.attackXY(X,Y);
+this.attackXY(X,Y);
+this.attackXY(X,Y);
+this.attackXY(X,Y);
+
+*********************************************
+// When you are finished with all of the campaigns, answer the following questions. You may want to look at some resources on JavaScript before answering.
+//   - What is `this` referring to? Think programming-wise rather than in the terms of the game. 
+//   - What does the `()` do in JavaScript?
+//   - What is the point of the semicolons?
  
- 
- 
- 
- 
- 
- 
+// 1. 'this' is referring to an object. So everytime I invoked this I was calling that object. 
+// 2. '()' in JavaScript is all about a method. whatever is in the parantheses is a method where the input (parameters) go.
+// 3. semicolons are the indicator of the end of that piece of code. 
+
+
 // Reflection:
 // Write your reflection here.
+<p>
+This exercise was really time consuming and essentially repetitive. There is some learning going on especially 
+learnign the syntax of JavaScript. In the end I think it was important to really get some practice in with Javascript.  
+I needed to try to save this again and try again. 
+New problem: when i try to commit: fatal: Unable to creat c:/Sites/ etc. .git/index.lock file exists.
+If no other git process is currently running, this probably means a git process crashed in this repository earlier. Make sure no 
+other git process is running and remove the file manually to continue. 
+</p>
